0829f627f9b93b869ec34ba78503e874a8427336
hagijerri <75515196+hagijerri@users.noreply.github.com>
2021-05-13 16:56:37 -0700

    Update txt-img.py

--- txt-img.py
+++ txt-img.py
@@ -1,9 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
+
+from cs50 import get_string
  
+#image color, asking the text, and font
 img = Image.new('RGB', (100, 30), color = (73, 109, 137))
+
+text = get_string("text:")
  
 fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
 d = ImageDraw.Draw(img)
-d.text((10,10), "Hello world", font=fnt, fill=(255, 255, 0))
+d.text((10,10), text , font=fnt, fill=(255, 255, 0))
  
 img.save('pil_text_font.png')
